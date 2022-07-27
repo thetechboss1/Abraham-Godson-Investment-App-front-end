067ae1fae8bce3d87620f0d2dfdb25341cad43b1
Godswillcode <omenukoonyekachi690@gmail.com>
2022-07-28 00:32:02 +0100

    login

--- src/Auth/Login.js
+++ src/Auth/Login.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import logo from "../Images/logo1.png"
 
 const Login = () => {
   return (
@@ -6,14 +7,17 @@ const Login = () => {
         <div className='bg-white shadow-md rounded-md'>
             <div className='px-10 py-5 flex items-center'>
                 <div>
+                    <div className='mb-5'>
+                        <img src={logo} alt="logo" className='h-12'/>
+                    </div>
                     <form>
                         <div className='form-group'>
                              <label>Email</label>
                              <input type="email" placeholder='johndoe@gmail'/>
                         </div>
-                        <div className='form-group'>
-                             <label>Email</label>
-                             <input type="email" placeholder='johndoe@gmail'/>
+                        <div className='form-group mt-3'>
+                             <label>Password</label>
+                             <input type="password" placeholder='Enter password'/>
                         </div>
                         <button type='submit'>Sign In</button>
 
