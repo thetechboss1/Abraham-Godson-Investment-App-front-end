32a4f60cc70799c09de97fc00aab861fcdd228f3
Godswillcode <omenukoonyekachi690@gmail.com>
2022-08-04 21:40:39 +0100

    Update CreateHouse.js

--- src/Components/CreateHouse.js
+++ src/Components/CreateHouse.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Modal } from "@mui/material";
+import React from "react";
 
-const CreateHouse = () => {
+const CreateHouse = ({ handleClose, open }) => {
   return (
-    <div>CreateHouse</div>
-  )
-}
+    <Modal open={open}>
+      <div className="CModal" style={{ maxWidth: 450 }}>
+        <div className="flex items-center justify-between w-full mb-7">
+          <h5 className="text-lg font-semibold text-accent">
+            Share Property via
+          </h5>
+          <i
+            className="fas fa-times cursor-pointer text-xl"
+            onClick={handleClose}
+          ></i>
+        </div>
 
-export default CreateHouse
+        <div></div>
+      </div>
+    </Modal>
+  );
+};
+
+export default CreateHouse;
