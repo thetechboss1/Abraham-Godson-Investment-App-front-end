cb0cdc1af1ff26c3ce8333280e385d9ebd2e29e1
Godswillcode <omenukoonyekachi690@gmail.com>
2022-08-17 07:45:27 +0100

    Update ShareProperty.js

--- src/Components/ShareProperty.js
+++ src/Components/ShareProperty.js
@@ -1,8 +1,11 @@
+import { Face } from "@mui/icons-material";
 import { Modal } from "@mui/material";
 import React from "react";
+import { FacebookShareButton, FacebookIcon } from "react-share";
 
 const ShareProperty = ({ handleClose, open }) => {
-    const styleButton = "h-12 w-12 flex justify-center items-center rounded-full text-white cursor-pointer hover:shadow-lg"
+  const styleButton =
+    "h-12 w-12 flex justify-center items-center rounded-full text-white cursor-pointer hover:shadow-lg";
   return (
     <>
       <Modal open={open} onClose={handleClose}>
@@ -17,11 +20,18 @@ const ShareProperty = ({ handleClose, open }) => {
             ></i>
           </div>
           <div className="flex items-center gap-5 flex-wrap justify-center">
-            <i className={`ri-twitter-line text-3xl bg-blue-400 ${styleButton }`}></i>
+            <FacebookShareButton
+              url="http://localhost:3000/properties"
+              quote="Property"
+              hashtag="#React"
+            >
+              <FacebookIcon size={32} round={true} />
+            </FacebookShareButton>
+            {/* <i className={`ri-twitter-line text-3xl bg-blue-400 ${styleButton }`}></i>
             <i className={`ri-facebook-fill text-3xl bg-blue-600 ${styleButton }`}></i>
             <i className={`ri-whatsapp-line text-3xl bg-green-500 ${styleButton }`}></i>
             <i className={`ri-linkedin-fill text-3xl bg-blue-500 ${styleButton }`}></i>
-            <i className={`ri-telegram-line text-3xl bg-blue-400 ${styleButton }`}></i>
+            <i className={`ri-telegram-line text-3xl bg-blue-400 ${styleButton }`}></i> */}
           </div>
         </div>
       </Modal>
