250f5da4dab5e4e134e1a78f75d505637ff4861b
Godswillcode <omenukoonyekachi690@gmail.com>
2022-08-18 20:36:00 +0100

    react-share

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
+import { BrowserRouter } from "react-router-dom";
+import PageContextProvider from "./Context/PageContextProvider";
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-import PageContextProvider from "./Context/PageContextProvider";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <PageContextProvider>
-        <App />
-      </PageContextProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+ReactDOM.render(
+  <BrowserRouter>
+    <PageContextProvider>
+      <App />,
+    </PageContextProvider>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
